8ed7c9461e80ca9ffad465343b260dfca7f9a527
Alex Kareev <akareev@uark.edu>
2018-04-21 12:59:08 -0500

    First Commit at 12:58pm

---
+++ HTMLForm/scripts/main.js
@@ -0,0 +1,185 @@
+$(function(){
+   
+
+   $('#btnSubmit').on('click', submit);
+   
+   $('#resetButton').on('click', resetFunction);
+
+
+
+
+    function submit(){
+		//debugger;
+		
+		$("#firstName").removeClass("invalid");
+		$("#lastName").removeClass("invalid");
+		$("#eMail").removeClass("invalid");
+		$("#howYouFindUS").removeClass("invalid");
+		$("#whyBlender").removeClass("invalid");
+		$("#termsAndConditions").removeClass("invalid");
+		//$("#termsAndConditions").classList.remove("invalid");
+		
+		//document.getElementById('eMail').classList.remove("invalid");
+  //document.getElementById('eMail').classList.remove("invalid");
+  //document.getElementById('password').classList.remove("invalid");
+  //document.getElementById('timeZone').classList.remove("invalid");
+      //document.getElementById('termsAndConditions').classList.remove("invalid");
+
+   errorArray=[];
+   //$("#message").html("");
+  document.getElementById('message').innerHTML="";
+  //alert("Hello");
+  var firstNameValue=document.getElementById('firstName').value;
+    //var firstNameValue=$("firstName").val();
+	//console.log(firstNameValue);
+	var lastNameValue=document.getElementById('lastName').value;
+	//var lastNameValue=$("lastName").val();
+	//console.log(lastNameValue);
+  var emailAddressValue=document.getElementById('eMail').value;
+ // var emailAddressValue=$("eMail").val();
+	//console.log(emailAddressValue);
+	 
+var howYouFindUsVal=document.getElementById('howYouFindUS').value;
+ //var howYouFindUsVal=$("howYouFindUS").val();
+	//console.log(howYouFindUsVal);
+	
+	
+  //var timeZoneValue=document.getElementById('timeZone').value;
+    var whyBlenderVal=document.getElementById('whyBlender').value;
+  //var whyBlenderVal=$("whyBlender").val();
+	//console.log(whyBlenderVal);
+  
+  
+  
+  var termsAndConditionChecked=document.getElementById('termsAndConditions').checked;
+  
+  var firstNameFilledOut=(firstNameValue==="") ? false:true ;
+   var lastNameFilledOut=(lastNameValue==="") ? false:true ;
+  var emailFilledOut=(emailAddressValue==="") ? false:true ;
+  var emailValid=(emailAddressValue.indexOf("@")>0) ? true:false ;
+  var emailValid2=(emailAddressValue.indexOf(".com")>0) ? true:false ;
+  
+  //var passwordFilledOut=(passwordValue==="" || passwordValue.length <=3) ? false:true ;
+  var howYouFindSelected=(howYouFindUsVal==="") ? false:true ;
+  var whyBlenderFilledOut=(whyBlenderVal==="") ? false:true ;
+  
+  if(!firstNameFilledOut){
+     //alert("No User Name");
+    //document.getElementById('Name').classList.add("invalid");
+	$("#firstName").addClass("invalid");
+    errorArray.push("First Name must be filled out");
+     }
+	 
+	 
+	  if(!lastNameFilledOut){
+     //alert("No User Name");
+    //document.getElementById('Name').classList.add("invalid");
+		$("#lastName").addClass("invalid");
+    errorArray.push("Last Name must be filled out");
+     }
+  if(!emailFilledOut){
+     //alert("No email");
+    //document.getElementById('eMail').classList.add("invalid");
+	$("#eMail").addClass("invalid");
+    errorArray.push("Email must be filled out");
+     }
+  
+    if(!emailValid || !emailValid2){
+     //alert("email not valid");
+     // document.getElementById('eMail').classList.add("invalid");
+	 $("#eMail").addClass("invalid");
+      errorArray.push("Email must contain '@' and '.com'");
+     }
+  
+   
+  
+     if(!howYouFindSelected){
+     //alert("Time zone Not selected");
+       //document.getElementById('timeZone').classList.add("invalid");
+	   $("#howYouFindUS").addClass("invalid");
+       errorArray.push("A value must be selected in the \'How did you find us\" drop down");
+     }
+	 
+	 
+	  if(!whyBlenderFilledOut){
+     //alert("Time zone Not selected");
+       //document.getElementById('timeZone').classList.add("invalid");
+	   $("#whyBlender").addClass("invalid");
+       errorArray.push("\'Why do you need this blender\' must be filled out");
+     }
+  
+   if(!termsAndConditionChecked){
+    // alert("Need to Accept Terms and Conditions");
+ document.getElementById('termsAndConditions').classList.add("invalid");
+     errorArray.push("The terms and service checkbox must be checked.");
+     }
+  
+       if(firstNameFilledOut &&lastNameFilledOut && emailFilledOut && emailValid && emailValid2 && howYouFindSelected && whyBlenderFilledOut && termsAndConditionChecked ){
+        alert("Thank you for submiting the form!");
+;
+        }
+  
+  for(var i=0;i<errorArray.length;i++){document.getElementById('message').innerHTML+="<p>"+ errorArray[i] +"</p>";}
+  //for(var i=0;i<errorArray.length;i++){$("#message").html()+="<p>"+ errorArray[i] +"</p>";}
+
+    }
+	
+	
+	
+	function resetFunction(){
+     errorArray=[];
+  
+  /*document.getElementById('Name').classList.remove("invalid");  
+  document.getElementById('eMail').classList.remove("invalid");
+  document.getElementById('password').classList.remove("invalid");
+  document.getElementById('timeZone').classList.remove("invalid");
+  document.getElementById('termsAndConditions').classList.remove("invalid");*/
+  $("#firstName").removeClass("invalid");
+  $("#lastName").removeClass("invalid");
+  $("#eMail").removeClass("invalid");
+  $("#howYouFindUS").removeClass("invalid");
+  $("#whyBlender").removeClass("invalid");
+  
+ // alert("World");
+  
+ 
+ /*
+  var firstNameValue=$("firstName").val("");
+	console.log(firstNameValue);
+	
+	var lastNameValue=$("lastName").val()="";
+	console.log(lastNameValue);
+  //var emailAddressValue=document.getElementById('eMail').value;
+  var emailAddressValue=$("eMail").val='';
+	console.log(emailAddressValue);
+	
+ var howYouFindUsVal=$("howYouFindUS").val()="";
+	console.log(howYouFindUsVal);
+	
+	
+  //var timeZoneValue=document.getElementById('timeZone').value;
+  
+  var whyBlenderVal=$("whyBlender").val()="";
+	console.log(whyBlenderVal);
+  
+   document.getElementById('termsAndConditions').checked=false;
+   */
+   
+   
+   document.getElementById('message').innerHTML="";
+  
+   document.getElementById('firstName').value="";
+   document.getElementById('lastName').value="";
+ document.getElementById('eMail').value="";
+ document.getElementById('howYouFindUS').value="";
+  document.getElementById('whyBlender').value="";
+ document.getElementById('termsAndConditions').checked=false;
+  
+}
+
+
+	
+	
+	
+	
+});
---
+++ debug/scripts/main.js
@@ -0,0 +1,347 @@
+/*
+    Read the comments and follow the instructions
+    Total Points available -> 80
+    Part 1 - 30 points
+    Part 2 - 40 points
+    Clean Code and Comments - 10 points
+    Total Challenge/Bonus points -> 10
+*/
+
+
+/*========================
+    Part 1 Fixing Code - 30
+====================================*/
+
+
+/*
+    The code below is broken and not doing what it should.  
+    
+    Do your best to fix it.
+    Look for:
+    Wrong selectors
+    Misspelling
+    Variables used at the wrong time
+    Wrong operators
+    It uses the HTML in /debugging/index.html  
+    Points Available: 30points
+*/
+$(function () {
+
+
+    //This is supposed to add an event listener but it's causing an error
+    //Fix it please.
+    //document.getElementById('btnMake').addEventListener('click', build);
+    $('#btnMake').on('click', build);
+
+    function build() {
+		//debugger;
+        var firstName = $('#firstName');
+        var firstNameValue = firstName.val('Whoops');
+
+
+        var lastName = $('#lastName');
+        var lastNameValue = lastName.val("");
+
+        /*invalid should add a red border around the respective input
+            It will also show a hidden error message */
+        if (firstNameValue === '') {
+            firstName.addClass('invalid');
+
+            var parentDiv1 = firstName.closest('.input-group');
+            var errorSpan1 = parentDiv1.find('.errors');
+            errorSpan1.show();
+        }
+
+        if (lastNameValue ==='') {
+            lastName.addClass('invalid');
+
+            var parentDiv2 = lastName.closest('.input-group');
+            var errorSpan2= parentDiv2.find('.errors');
+            errorSpan2.show();
+        }
+
+
+        /*====================================
+            I need to create an object, but it's not working
+            so I just commented it out.
+            Please fix it
+        ======================================*/
+
+		
+        var userObject = {
+            firstName: "Cat" ,
+            lastName:"Kat" ,
+            fullName: function() {
+                return this.firstName + ' ' + this.lastName;
+            },
+            state: $('#state').get()
+        };
+		
+		console.log(userObject.fullName());
+        console.log(userObject);
+		
+
+        
+
+
+    }
+
+
+
+
+
+});
+
+
+
+/*========================
+    Part 2 Writing Functions -  10 points each
+====================================*/
+
+
+/*
+    Write a JavaScript function that accepts 
+    one input parameter and returns the number 
+    of characters in the string.
+    //Sample
+    var foo = getLength('hello world');
+    console.log(foo) // Outputs 11
+*/
+
+/*
+Write getLength function here
+
+*/
+function getLength(input){
+	
+	return input.length;
+}
+
+// Uncomment this to test getLength function
+console.log('-------------Get Length----------------'); 
+var foo = getLength('hello world');
+console.log(foo); // Outputs 11
+var foo2 = getLength('lorem lorem asldkf asldfkja sdf');
+console.log(foo2); // Outputs 31
+console.log('-----------------------------'); 
+
+
+
+
+
+/*
+    Write a JavaScript function that computes the sum
+    all of the numbers in an array.
+    //Sample
+    var foo = sumArray([2,5,6]);
+    console.log(foo) // Outputs 13
+*/
+
+/*
+Write sumArray function here
+*/
+//debugger;
+function sumArray(inputArray){
+	var arrayValue=0;
+	var temArray=inputArray;
+	for (i=0; i<temArray.length; i++)
+	{
+	arrayValue+=temArray[i];
+	//console.log(arrayValue);
+		
+	}
+	
+	return arrayValue;
+}
+
+// Uncomment this to test sumArray function
+console.log('-------------  SUM ARRAY ----------------'); 
+foo = sumArray([2, 5, 6]);
+console.log(foo); // Outputs 13
+
+foo = sumArray([2, 5, 6, 2, 5, 6]);
+console.log(foo); // Outputs 26
+
+console.log('-----------------------------');
+
+
+
+/*
+    Write a JavaScript function that converts a decimal
+    into a percentage.
+    //Sample
+    var foo = percentage(.23);
+    console.log(foo) //Outputs "23%"
+    var foo2 = percentage(.5);
+    console.log(foo2) //Outputs "50%"
+*/
+
+/*
+Write percentage function here
+*/
+
+function percentage(input){
+	var perc=input*100;
+	var text=perc+"%";
+	return text;
+	
+}
+
+// Uncomment this to test percentage function
+console.log('------------ Percentage-----------------');
+
+foo = percentage(.23);
+console.log(foo) //Outputs "23%"
+
+foo2 = percentage(.5);
+console.log(foo2) //Outputs "50%"
+
+console.log('-----------------------------');
+
+
+/*
+    Write a JavaScript function that takes a date and adds a 
+    number of days to it.
+    //Sample
+    var d = new Date('1/1/2001');
+    var newD = addDays(d, 10);
+    console.log(newD);  //outputs Thu Jan 11 2001 00:00:00 GMT-0600 (Central Standard Time)
+*/
+
+/*
+Write addDays function here
+
+*/
+
+function addDays(startDate, addDays) {
+  var result = new Date(startDate);
+  result.setDate(result.getDate() + addDays);
+  return result;
+}
+
+// Uncomment this to test addDays function
+var d = new Date('1/1/2001');
+var newD = addDays(d, 10);
+console.log(newD); //outputs Sun Jun 11 2001 00:00:00 GMT-0500 (Central Daylight Time)
+d = new Date('9/10/2001');
+newD = addDays(d, 1000);
+console.log(newD); //outputs Sun Jun 06 2004 00:00:00 GMT-0500 (Central Daylight Time)
+
+
+
+
+
+
+
+
+/*
+    Challenge - 
+    Write a javascript function to take an array of numbers
+    and sorts them in ascending order.
+    //Sample
+    var array1 = [3, 5, 1, 10];
+    console.log(sortArray(array1); //outputs [1,3,5,10]
+*/
+
+/*
+Write sortArray function here
+*/
+function sortArray(input){
+	
+	input.sort(function(a, b){return a - b});
+	return input; 
+}
+
+
+// Uncomment this to test sortArray function
+var array1 = [3, 5, 1, 10];
+console.log(sortArray(array1)); //outputs [1,3,5,10]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+=====================================================================
+    Nothing is broken here... here be dragons
+    Go away :)
+*/
+$(function () {
+    $('#btnReset').on('click', function () {
+        $('#firstName').val('John');
+        $('#lastName').val('Doe');
+        $('#state').val('OK');
+        $('.errors').hide();
+        $('.invalid').removeClass('invalid');
+    });
+});
